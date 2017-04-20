edd21bcb03852f9e1cc4d1de64782aa2511730f6
BaseShare <chacker@163.com>
2017-04-20 20:25:59 +0800

    新增加了日志处理程序

--- java-mongo/src/main/java/com/ctfo/cache/bean/BaseCacheBean.java
+++
@@ -1,5 +0,0 @@
-package com.ctfo.cache.bean;
-
-public class BaseCacheBean {
-
-}
--- java-mongo/src/main/java/com/ctfo/file/bean/DynamicMongoParameter.java
+++
@@ -1,9 +0,0 @@
-package com.ctfo.file.bean;
-
-import com.ctfo.csm.local.DynamicSqlParameter;
-
-public class DynamicMongoParameter extends DynamicSqlParameter {
-
-	private static final long serialVersionUID = -252740552779961891L;
-
-}
